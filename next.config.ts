2708dbd14ba0567129ab85c71f7f67cc75ccd77a
shifataj43-rgb <shifataj43@gmail.com>
2026-06-23 15:03:44 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Allow mobile testing on the local network
   allowedDevOrigins: ['192.168.1.12'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
